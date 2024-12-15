045b715eb369eb0fc9525fd06e38b752193bdde6
Андрей <Barbus1966@gmail.com>
2024-12-15 19:43:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (False, 'Телефон', 99, 44)
+print(immutable_var)
+# immutable_var [0] = True; ошибка, это неизменяемый тип данных
+
+mutable_list = [False, 'Телефон', 99, 44]
+mutable_list [0] = True
+print(mutable_list)
